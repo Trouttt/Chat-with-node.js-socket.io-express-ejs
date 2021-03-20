3ef77be392bc2cfb2ca9515bab716312051a7478
Trouttt <44469295+Trouttt@users.noreply.github.com>
2021-03-20 10:45:36 -0300

    Feature: disconnect chat

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
+io.on("connection", (socket) => { //socket é o cliente
+    socket.on("disconnect", () => {
+        console.log("X desconectou: " + socket.id);
+    });
+});
 
 app.set("view engine", 'ejs');
 
