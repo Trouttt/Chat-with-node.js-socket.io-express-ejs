ea785722b87456f915ae2377f4bedf3bd3268068
Trouttt <44469295+Trouttt@users.noreply.github.com>
2021-03-21 15:19:25 -0300

    Update: index's css

--- src/index.js
+++ src/index.js
@@ -7,6 +7,13 @@ io.on("connection", (socket) => { //socket é o cliente
     socket.on("disconnect", () => {
         console.log("X desconectou: " + socket.id);
     });
+
+    socket.on("msg", (data) => {
+        //socket.broadcast.emit() envia pra todo mundo, menos pro usuário q mandou(eu);
+        io.emit("show", data);// emite para pra todo mundo
+        //socket.emit("show", data); //não emite para todo mundo.
+        console.log(data);
+    })
 });
 
 app.set("view engine", 'ejs');
